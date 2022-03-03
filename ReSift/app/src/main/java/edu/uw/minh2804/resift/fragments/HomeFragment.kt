6c2876022ff0250a45d1537dd10aa4eaa2809441
Tom Nguyen <tomn2804@gmail.com>
2022-03-03 02:44:50 -0800

    Add share button

--- ReSift/app/src/main/java/edu/uw/minh2804/resift/fragments/HomeFragment.kt
+++ ReSift/app/src/main/java/edu/uw/minh2804/resift/fragments/HomeFragment.kt
@@ -1,6 +1,7 @@
 package edu.uw.minh2804.resift.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
--- ReSift/app/src/main/java/edu/uw/minh2804/resift/fragments/SiftResultFragment.kt
+++ ReSift/app/src/main/java/edu/uw/minh2804/resift/fragments/SiftResultFragment.kt
@@ -1,6 +1,36 @@
 package edu.uw.minh2804.resift.fragments
 
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import com.google.android.material.button.MaterialButton
 import edu.uw.minh2804.resift.R
+import edu.uw.minh2804.resift.viewmodels.ArticleViewModel
 
-class SiftResultFragment : Fragment(R.layout.fragment_sift_result)
+class SiftResultFragment : Fragment(R.layout.fragment_sift_result) {
+    private val viewModel: ArticleViewModel by activityViewModels()
+
+    private var sendIntent: Intent? = null
+    private var shareIntent: Intent? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.inputUrl.observe(viewLifecycleOwner) {
+            sendIntent = Intent().apply {
+                action = Intent.ACTION_SEND
+                type = "text/plain"
+                putExtra(Intent.EXTRA_TEXT, it)
+            }
+            shareIntent = Intent.createChooser(sendIntent, null)
+        }
+
+        view.findViewById<MaterialButton>(R.id.button_sift_result_share).setOnClickListener {
+            if (shareIntent != null) {
+                startActivity(shareIntent)
+            }
+        }
+    }
+}
