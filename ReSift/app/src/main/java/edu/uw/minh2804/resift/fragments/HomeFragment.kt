75a62253d4b6b0cd08634e66aa332d46c14cd21d
Tom Nguyen <tomn2804@gmail.com>
2022-04-17 00:19:33 -0700

    Add debug buttons

--- ReSift/app/src/main/java/edu/uw/minh2804/resift/fragments/HomeFragment.kt
+++ ReSift/app/src/main/java/edu/uw/minh2804/resift/fragments/HomeFragment.kt
@@ -14,10 +14,16 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
 
 	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 		super.onViewCreated(view, savedInstanceState)
-		view.findViewById<Button>(R.id.button_home_search).setOnClickListener {
-			//viewModel.siftArticle("https://www.reuters.com/world/europe/powerful-explosions-heard-kyiv-after-russian-warship-sinks-2022-04-15/")
+		view.findViewById<Button>(R.id.button_home_debug_search_default).setOnClickListener {
+			viewModel.siftArticle("https://www.reuters.com/world/europe/powerful-explosions-heard-kyiv-after-russian-warship-sinks-2022-04-15/")
+		}
+		view.findViewById<Button>(R.id.button_home_debug_search_error).setOnClickListener {
 			viewModel.siftArticle("https://www.reuters.com/world/europe/powerful-explos-2022-04-15/")
-			findNavController().navigate(R.id.action_HomeFragment_to_SiftFragment)
+		}
+		viewModel.isQuerying.observe(viewLifecycleOwner) {
+			if (it) {
+				findNavController().navigate(R.id.action_HomeFragment_to_SiftFragment)
+			}
 		}
 	}
 }
